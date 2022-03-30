e217586b890712e44a2d68d268c259b6e6f0b612
Jetvac <62834629+Jetvac@users.noreply.github.com>
2022-03-30 12:49:13 +0300

    Добавьте файлы проекта.

---
+++ Laboratory/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Laboratory.Models;
+using System.Windows;
+
+namespace Laboratory
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static LaboratoryEntities DBConnection = new LaboratoryEntities();
+    }
+}
---
+++ Laboratory/Views/Pages/Auth.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Laboratory.Models;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Laboratory.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AccountData user = App.DBConnection.AccountDatas.FirstOrDefault(c => c.Login == Login.Text && c.Password == Password.Password);
+            try
+            {
+                if (user != null)
+                {
+                    NavigationService.Navigate(new Pages.MainMenu());
+                    MessageBox.Show("Вы успешно авторизировались!");
+                }
+                else
+                {
+                    MessageBox.Show("Аккаунт не найден!");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка соединения с базой данных!");
+            }
+        }
+    }
+}
